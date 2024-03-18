bc913310efe07abfff20fb6cb121aff12b05fef8
azikulov <azikulov9@gmail.com>
2024-03-18 11:49:56 +0500

    changed pin

--- src/app/(main)/map/page.tsx
+++ src/app/(main)/map/page.tsx
@@ -25,7 +25,7 @@ export default function MapPage() {
 
   const pins = useMemo(
     () =>
-      Cities.map((city: any, index) => (
+      Cities.map((city: MapPin, index) => (
         <Marker
           key={`marker-${index}`}
           latitude={city.latitude}
@@ -72,6 +72,7 @@ export default function MapPage() {
           <NavigationControl position='top-left' />
           <ScaleControl />
 
+
           {pins}
 
           {popupInfo && (
@@ -84,29 +85,15 @@ export default function MapPage() {
             >
               <p className={styles['mapboxgl-popup__title']}>Дата: {popupInfo.date}</p>
               <p className={styles['mapboxgl-popup__title']}>Время: {popupInfo.time}</p>
-              <img className={styles['mapboxgl-popup__img']} width='100%' src={`/assets/photos/${popupInfo.photo}`} />
+              <img
+                className={styles['mapboxgl-popup__img']}
+                width='100%'
+                src={`/assets/photos/${popupInfo.photo}`}
+              />
             </Popup>
           )}
         </Map>
       </div>
     </div>
-  );
-}
-
-const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
-  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
-  C20.1,15.8,20.2,15.8,20.2,15.7z`;
-
-const pinStyle = {
-  cursor: 'pointer',
-  fill: '#d00',
-  stroke: 'none',
-};
-
-function Pin({ size = 20 }: any) {
-  return (
-    <svg height={size} viewBox='0 0 24 24' style={pinStyle}>
-      <path d={ICON} />
-    </svg>
   );
 }
